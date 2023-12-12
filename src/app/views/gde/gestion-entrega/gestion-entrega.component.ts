b303c8a1235918bacaf846cd4eeed2e474e3c751
jimmy <rootkaliente@gmail.com>
2023-12-12 10:15:06 -0500

    feat: fixed export files gde and gdo

--- src/app/views/gde/gestion-entrega/gestion-entrega.component.ts
+++ src/app/views/gde/gestion-entrega/gestion-entrega.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { GestionEntregaService, GestionEntrega } from '../../../services/gde/gestionEntrega/gestion-entrega.service';
-import { DatePipe } from '@angular/common';
-import { ParamService as ParamServiceADM } from 'src/app/services/admin/param.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GestionEntregaService, GestionEntrega} from '../../../services/gde/gestionEntrega/gestion-entrega.service';
+import {DatePipe} from '@angular/common';
+import {ParamService as ParamServiceADM} from 'src/app/services/admin/param.service';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ExportService} from '../../../services/admin/export.service';
 
 
 @Component({
@@ -19,26 +20,29 @@ export class GestionEntregaComponent implements OnInit {
   entregaForm: FormGroup;
 
   submittedEntrega = false;
-  mensaje = "";
+  mensaje = '';
   menu;
-  tipoCliente = "";
+  tipoCliente = '';
   identificacion;
   page = 1;
   pageSize: any = 10;
   maxSize;
   collectionSize;
-  fecha = "";
-  inicio = "";
-  fin = "";
+  fecha = '';
+  inicio = '';
+  fin = '';
   listaEntregas;
   entrega;
   ultimosProductos;
+  infoExportar = [];
+
   constructor(
     private gestionEntregaService: GestionEntregaService,
     private datePipe: DatePipe,
     private globalParam: ParamServiceADM,
     private _formBuilder: FormBuilder,
     private modalService: NgbModal,
+    private exportFile: ExportService,
   ) {
     this.entrega = this.gestionEntregaService.inicializarGestionEntrega();
   }
@@ -46,6 +50,7 @@ export class GestionEntregaComponent implements OnInit {
   get enForm() {
     return this.entregaForm.controls;
   }
+
   ngOnInit(): void {
     this.entregaForm = this._formBuilder.group({
       entregoProducto: ['', [Validators.required]],
@@ -55,16 +60,16 @@ export class GestionEntregaComponent implements OnInit {
       estado: ['', [Validators.required]],
     });
     this.menu = {
-      modulo: "gde",
-      seccion: "gestEntrega"
+      modulo: 'gde',
+      seccion: 'gestEntrega'
     };
     this.obtenerListaGestionEntrega();
   }
 
   obtenerListaGestionEntrega() {
     let fecha = this.fecha.split(' to ');
-    this.inicio = fecha[0] ? fecha[0] : "";
-    this.fin = fecha[1] ? fecha[1] : "";
+    this.inicio = fecha[0] ? fecha[0] : '';
+    this.fin = fecha[1] ? fecha[1] : '';
     let busqueda: any = {
       page: this.page - 1,
       page_size: this.pageSize,
@@ -72,9 +77,9 @@ export class GestionEntregaComponent implements OnInit {
       fin: this.fin
     };
     if (this.tipoCliente == 'negocio') {
-      busqueda = { ...busqueda, negocio: 1, identificacion: this.identificacion }
+      busqueda = {...busqueda, negocio: 1, identificacion: this.identificacion};
     } else if (this.tipoCliente == 'cliente') {
-      busqueda = { ...busqueda, cliente: 1, identificacion: this.identificacion }
+      busqueda = {...busqueda, cliente: 1, identificacion: this.identificacion};
     }
     this.gestionEntregaService.obtenerListaGestionEntrega(
       busqueda
@@ -83,19 +88,23 @@ export class GestionEntregaComponent implements OnInit {
       this.collectionSize = info.cont;
     });
   }
+
   transformarFecha(fecha) {
     let nuevaFecha = this.datePipe.transform(fecha, 'yyyy-MM-dd');
     return nuevaFecha;
   }
+
   primeraLetra(nombre, apellido) {
     let iniciales = nombre.charAt(0) + apellido.charAt(0);
     return iniciales;
   }
+
   obternerGestionEntrega(id) {
     this.gestionEntregaService.obtenerGestionEntrega(id).subscribe((info) => {
       this.entrega = info;
     });
   }
+
   obtenerUltimosProductos(id) {
     this.gestionEntregaService.obtenerUltimosProductos(id).subscribe((info) => {
       info.map((prod) => {
@@ -104,36 +113,73 @@ export class GestionEntregaComponent implements OnInit {
       this.ultimosProductos = info;
     });
   }
+
   obtenerURLImagen(url) {
     return this.globalParam.obtenerURL(url);
   }
+
   actualizarGestionEntrega() {
     this.submittedEntrega = true;
     if (this.entregaForm.invalid) {
       return;
     }
     this.gestionEntregaService.actualizarGestionEntrega(this.entrega).subscribe(() => {
-      this.obtenerListaGestionEntrega();
-      this.mensaje = "Entrega guardada";
-      this.abrirModal(this.aviso);
-      this.dismissModal.nativeElement.click();
-      this.submittedEntrega = false;
-    },
+        this.obtenerListaGestionEntrega();
+        this.mensaje = 'Entrega guardada';
+        this.abrirModal(this.aviso);
+        this.dismissModal.nativeElement.click();
+        this.submittedEntrega = false;
+      },
       (error) => {
         let errores = Object.values(error);
         let llaves = Object.keys(error);
-        this.mensaje = "";
+        this.mensaje = '';
         errores.map((infoErrores, index) => {
-          this.mensaje += llaves[index] + ": " + infoErrores + "<br>";
+          this.mensaje += llaves[index] + ': ' + infoErrores + '<br>';
         });
         this.abrirModal(this.aviso);
       });
 
   }
+
   cerrarModal() {
     this.modalService.dismissAll();
   }
+
   abrirModal(modal) {
     this.modalService.open(modal);
+  }
+
+  exportarExcel() {
+    this.infoExportar = [];
+    const headers = ['Código', 'Fecha de oferta', 'Nombre Cliente', 'Identificación', '#Contacto', 'Correo', 'Indicador de Cliente',
+      'Fecha de compra', '¿Se entregó el producto?', 'Fecha de entrega', 'Canal de ventas', 'Calificación', 'Monto USD', 'Estado de entrega'
+    ];
+    const objetoExportar = Object.create(this.listaEntregas);
+    objetoExportar.forEach((row: any) => {
+      const values = [];
+      values.push(row['id']);
+      values.push(this.transformarFecha(row['fechaOferta']));
+      values.push(`${row['nombres']} ${row['apellidos']}`);
+      values.push(row['identificacion']);
+      values.push(row['telefono']);
+      values.push(row['correo']);
+      values.push(row['indicadorCliente']);
+      values.push(row['fechaCompra']);
+      values.push(row['entregoProducto']);
+      values.push(row['fechaCompra']);
+      values.push(row['canalVentas']);
+      values.push(row['calificacion']);
+      values.push(row['total']);
+      values.push(row['estado']);
+      this.infoExportar.push(values);
+    });
+    const reportData = {
+      title: 'Reporte de Gestión de la entrega',
+      data: this.infoExportar,
+      headers
+    };
+
+    this.exportFile.exportExcel(reportData);
   }
 }
--- src/app/views/gdo/gestion-oferta/gestion-oferta.component.ts
+++ src/app/views/gdo/gestion-oferta/gestion-oferta.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { GestionOfertaService, GestionOferta } from '../../../services/gdo/gestionOferta/gestion-oferta.service';
-import { DatePipe } from '@angular/common';
-import { ParamService as ParamServiceADM } from 'src/app/services/admin/param.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {GestionOfertaService, GestionOferta} from '../../../services/gdo/gestionOferta/gestion-oferta.service';
+import {DatePipe} from '@angular/common';
+import {ParamService as ParamServiceADM} from 'src/app/services/admin/param.service';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {ExportService} from '../../../services/admin/export.service';
 
 @Component({
   selector: 'app-gestion-oferta',
@@ -16,35 +17,39 @@ export class GestionOfertaComponent implements OnInit {
 
   ofertaForm: FormGroup;
 
-  submittedOferta = false;  
+  submittedOferta = false;
 
-  mensaje = "";
+  mensaje = '';
   menu;
-  tipoCliente = "";
+  tipoCliente = '';
   identificacion;
   page = 1;
   pageSize: any = 10;
   maxSize;
   collectionSize;
-  fecha = "";
-  inicio = "";
-  fin = "";
+  fecha = '';
+  inicio = '';
+  fin = '';
   listaOfertas;
   ultimosProductos;
   oferta;
+  infoExportar = [];
+
   constructor(
     private gestionOfertaService: GestionOfertaService,
     private datePipe: DatePipe,
     private globalParam: ParamServiceADM,
     private _formBuilder: FormBuilder,
     private modalService: NgbModal,
-
+    private exportFile: ExportService,
   ) {
     this.oferta = this.gestionOfertaService.inicializarGestionOferta();
   }
+
   get ofForm() {
     return this.ofertaForm.controls;
   }
+
   ngOnInit(): void {
     this.ofertaForm = this._formBuilder.group({
       comunicoOferta: ['', [Validators.required]],
@@ -55,23 +60,26 @@ export class GestionOfertaComponent implements OnInit {
       estado: ['', [Validators.required]],
     });
     this.menu = {
-      modulo: "gdo",
-      seccion: "gestOferta"
+      modulo: 'gdo',
+      seccion: 'gestOferta'
     };
     this.obtenerListaGestionOferta();
   }
+
   ngAfterViewInit() {
 
   }
+
   obternerGestionOferta(id) {
     this.gestionOfertaService.obtenerGestionOferta(id).subscribe((info) => {
       this.oferta = info;
     });
   }
+
   obtenerListaGestionOferta() {
     let fecha = this.fecha.split(' to ');
-    this.inicio = fecha[0] ? fecha[0] : "";
-    this.fin = fecha[1] ? fecha[1] : "";
+    this.inicio = fecha[0] ? fecha[0] : '';
+    this.fin = fecha[1] ? fecha[1] : '';
     let busqueda: any = {
       page: this.page - 1,
       page_size: this.pageSize,
@@ -79,9 +87,9 @@ export class GestionOfertaComponent implements OnInit {
       fin: this.fin
     };
     if (this.tipoCliente == 'negocio') {
-      busqueda = { ...busqueda, negocio: 1, identificacion: this.identificacion }
+      busqueda = {...busqueda, negocio: 1, identificacion: this.identificacion};
     } else if (this.tipoCliente == 'cliente') {
-      busqueda = { ...busqueda, cliente: 1, identificacion: this.identificacion }
+      busqueda = {...busqueda, cliente: 1, identificacion: this.identificacion};
     }
     this.gestionOfertaService.obtenerListaGestionOferta(
       busqueda
@@ -90,14 +98,17 @@ export class GestionOfertaComponent implements OnInit {
       this.collectionSize = info.cont;
     });
   }
+
   transformarFecha(fecha) {
     let nuevaFecha = this.datePipe.transform(fecha, 'yyyy-MM-dd');
     return nuevaFecha;
   }
+
   primeraLetra(nombre, apellido) {
     let iniciales = nombre.charAt(0) + apellido.charAt(0);
     return iniciales;
   }
+
   obtenerUltimosProductos(id) {
     this.gestionOfertaService.obtenerUltimosProductos(id).subscribe((info) => {
       info.map((prod) => {
@@ -106,35 +117,76 @@ export class GestionOfertaComponent implements OnInit {
       this.ultimosProductos = info;
     });
   }
+
   obtenerURLImagen(url) {
     return this.globalParam.obtenerURL(url);
   }
-  actualizarGestionOferta(){
+
+  actualizarGestionOferta() {
     this.submittedOferta = true;
     if (this.ofertaForm.invalid) {
       return;
     }
-    this.gestionOfertaService.actualizarGestionOferta(this.oferta).subscribe(()=>{
-      this.obtenerListaGestionOferta();
-      this.mensaje = "Oferta guardada";
-      this.abrirModal(this.aviso);
-      this.dismissModal.nativeElement.click();
-      this.submittedOferta = false;
-    },
-    (error) => {
-      let errores = Object.values(error);
-      let llaves = Object.keys(error);
-      this.mensaje = "";
-      errores.map((infoErrores, index) => {
-        this.mensaje += llaves[index] + ": " + infoErrores + "<br>";
+    this.gestionOfertaService.actualizarGestionOferta(this.oferta).subscribe(() => {
+        this.obtenerListaGestionOferta();
+        this.mensaje = 'Oferta guardada';
+        this.abrirModal(this.aviso);
+        this.dismissModal.nativeElement.click();
+        this.submittedOferta = false;
+      },
+      (error) => {
+        let errores = Object.values(error);
+        let llaves = Object.keys(error);
+        this.mensaje = '';
+        errores.map((infoErrores, index) => {
+          this.mensaje += llaves[index] + ': ' + infoErrores + '<br>';
+        });
+        this.abrirModal(this.aviso);
       });
-      this.abrirModal(this.aviso);
-    });
   }
+
   cerrarModal() {
     this.modalService.dismissAll();
   }
+
   abrirModal(modal) {
     this.modalService.open(modal);
+  }
+
+  exportarExcel() {
+    this.infoExportar = [];
+    const headers = ['Código', 'Fecha de oferta', 'Nombre Cliente', 'Identificación', '#Contacto', 'Correo', 'Indicador de Cliente',
+      'Fecha de compra', 'Comunico la oferta', 'Fecha de comunicación', 'Acepto la oferta', 'FECHA DE ACEPTACIÓN', 'FECHA DE ACEPTACIÓN',
+      'CALIFICACIÓN DE LA OFERTA', 'Vigencia de la oferta', 'Canal de ventas', 'Monto USD', 'Estado'
+    ];
+    const objetoExportar = Object.create(this.listaOfertas);
+    objetoExportar.forEach((row: any) => {
+      const values = [];
+      values.push(row['id']);
+      values.push(this.transformarFecha(row['fechaOferta']));
+      values.push(`${row['nombres']} ${row['apellidos']}`);
+      values.push(row['identificacion']);
+      values.push(row['telefono']);
+      values.push(row['correo']);
+      values.push(row['indicadorCliente']);
+      values.push(row['fechaCompra']);
+      values.push(row['comunicoOferta']);
+      values.push(row['fechaComunicacion']);
+      values.push(row['aceptoOferta']);
+      values.push(row['fechaAceptacion']);
+      values.push(row['calificacionOferta']);
+      values.push(row['vigenciaOferta']);
+      values.push(row['canal']);
+      values.push(row['total']);
+      values.push(row['estado']);
+      this.infoExportar.push(values);
+    });
+    const reportData = {
+      title: 'Reporte de Gestión de la oferta',
+      data: this.infoExportar,
+      headers
+    };
+
+    this.exportFile.exportExcel(reportData);
   }
 }
