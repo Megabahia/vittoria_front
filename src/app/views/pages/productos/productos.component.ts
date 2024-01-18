719f15a1d4bda4a9d94696c1143399577cc78ab2
jimmy <rootkaliente@gmail.com>
2024-01-18 11:28:09 -0500

    creacion del formulario prospecto cliente en la pantalla productos por url

--- src/app/views/pages/productos/productos.component.ts
+++ src/app/views/pages/productos/productos.component.ts
@@ -1,20 +1,41 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {ProductosService} from '../../../services/gdp/productos/productos.service';
 import {ActivatedRoute} from '@angular/router';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ParamService} from '../../../services/mdm/param/param.service';
+import {ValidacionesPropias} from '../../../utils/customer.validators';
+import {ProspectosService} from '../../../services/mdm/prospectosCli/prospectos.service';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent implements OnInit {
-  @ViewChild('errorAuth') errorAuth;
+export class ProductosComponent implements OnInit, AfterViewInit {
+  @ViewChild('dismissModal') dismissModal;
+  @ViewChild('mensajeModal') mensajeModal;
+  public mensaje = '';
   producto;
+  pospectoForm: FormGroup;
+  submitted = false;
+  numRegex = /^-?\d*[.,]?\d{0,2}$/;
+  cargando = false;
+  canalOpciones;
+  paisOpciones;
+  provinciaOpciones;
+  ciudadOpciones;
+  vendedorOpciones;
+  confirmProspectoOpciones;
+  tipoPrecioOpciones;
+  tipoClienteOpciones;
 
   constructor(
     private rutaActiva: ActivatedRoute,
     private modalService: NgbModal,
+    private _formBuilder: FormBuilder,
+    private paramService: ParamService,
+    private prospectosService: ProspectosService,
     private productoService: ProductosService,
   ) {
     const navbar = document.getElementById('navbar');
@@ -25,13 +46,133 @@ export class ProductosComponent implements OnInit {
     }
   }
 
+  get f() {
+    return this.pospectoForm.controls;
+  }
+
   ngOnInit(): void {
     this.productoService.obtenerProductoFree(this.rutaActiva.snapshot.params.id).subscribe((info) => {
       this.producto = info;
     });
+    this.pospectoForm = this._formBuilder.group({
+      nombres: ['', [Validators.required]],
+      apellidos: ['', [Validators.required]],
+      telefono: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(10)]],
+      identificacion: ['', [
+        Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10),
+        Validators.maxLength(10), ValidacionesPropias.cedulaValido
+      ]],
+      tipoCliente: ['', [Validators.required]],
+      whatsapp: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(10), Validators.maxLength(10)]],
+      facebook: ['', [Validators.required]],
+      twitter: ['', [Validators.required]],
+      instagram: ['', [Validators.required]],
+      correo1: ['', [Validators.required, Validators.email]],
+      correo2: ['', [Validators.required, Validators.email]],
+      pais: ['', [Validators.required]],
+      provincia: ['', [Validators.required]],
+      ciudad: ['', [Validators.required]],
+      canal: ['', [Validators.required]],
+      nombreProducto: ['', [Validators.required]],
+      precio: ['', [Validators.required, Validators.pattern(this.numRegex)]],
+    });
   }
 
-  openModal(modal): void {
+  ngAfterViewInit(): void {
+    this.obtenerCanales();
+    this.obtenerProspectos();
+    this.obtenerVendedores();
+    this.obtenerTiposPrecio();
+    this.obtenerTiposCliente();
+    this.obtenerPaisOpciones();
+  }
+
+  obtenerCanales(): void {
+    this.paramService.obtenerListaPadres('CANAL').subscribe((info) => {
+      this.canalOpciones = info;
+    });
+  }
+
+  obtenerPaisOpciones(): void {
+    // this.provincia = '';
+    // this.ciudad = '';
+    this.paramService.obtenerListaPadres('PAIS').subscribe((info) => {
+      this.paisOpciones = info;
+    });
+  }
+
+  obtenerProvincias(): void {
+    // this.ciudad = '';
+    this.paramService.obtenerListaHijos(this.pospectoForm.value.pais, 'PAIS').subscribe((info) => {
+      this.provinciaOpciones = info;
+    });
+  }
+
+  obtenerCiudad(): void {
+    this.paramService.obtenerListaHijos(this.pospectoForm.value.provincia, 'PROVINCIA').subscribe((info) => {
+      this.ciudadOpciones = info;
+    });
+  }
+
+  obtenerVendedores(): void {
+    // this.prospectosService.obtenerVendedor('Vendedor').subscribe((info) => {
+    //   this.vendedorOpciones = info;
+    // });
+  }
+
+  obtenerProspectos(): void {
+    this.paramService.obtenerListaPadres('CONFIRMACION_PROSPECTO').subscribe((info) => {
+      this.confirmProspectoOpciones = info;
+    });
+  }
+
+  obtenerTiposPrecio(): void {
+    this.paramService.obtenerListaPadres('TIPO_PRECIO').subscribe((info) => {
+      this.tipoPrecioOpciones = info;
+    });
+  }
+
+  obtenerTiposCliente(): void {
+    this.paramService.obtenerListaPadres('TIPO_CLIENTE').subscribe((info) => {
+      this.tipoClienteOpciones = info;
+    });
+  }
+
+  crearProspecto(): void {
+    this.pospectoForm.get('nombreProducto').setValue(this.producto.titulo);
+    this.pospectoForm.get('precio').setValue(this.producto.precio);
+    this.submitted = true;
+    if (this.pospectoForm.invalid) {
+      console.log('form', this.pospectoForm);
+      return;
+    }
+    this.cargando = true;
+    this.prospectosService.crearProspectos(this.pospectoForm.value).subscribe(() => {
+        this.dismissModal.nativeElement.click();
+        this.submitted = false;
+        this.cargando = false;
+        this.mensaje = 'Prospecto creado exitosamente';
+        this.abrirModal(this.mensajeModal);
+        this.pospectoForm.reset();
+      },
+      (error) => {
+        const errores = Object.values(error);
+        const llaves = Object.keys(error);
+        this.mensaje = '';
+        errores.map((infoErrores, index) => {
+          this.mensaje += llaves[index] + ': ' + infoErrores + '<br>';
+        });
+        this.abrirModal(this.mensajeModal);
+        this.cargando = false;
+      }
+    );
+  }
+
+  abrirModal(modal): void {
     this.modalService.open(modal);
+  }
+
+  cerrarModal(): void {
+    this.modalService.dismissAll();
   }
 }
