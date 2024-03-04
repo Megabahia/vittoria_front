1278ed530d223a5a9417a40743272a0899c69205
jimmy <rootkaliente@gmail.com>
2024-03-04 16:40:26 -0500

    [fixed]: screen pedidos.component.html

--- src/app/views/mp/pedidos/pedidos.component.ts
+++ src/app/views/mp/pedidos/pedidos.component.ts
@@ -4,6 +4,8 @@ import {Color, Label} from 'ng2-charts';
 import {DatePipe} from '@angular/common';
 import {PedidosService} from '../../../services/mp/pedidos/pedidos.service';
 import {ParamService} from '../../../services/mp/param/param.service';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pedidos',
@@ -12,6 +14,8 @@ import {ParamService} from '../../../services/mp/param/param.service';
   providers: [DatePipe]
 })
 export class PedidosComponent implements OnInit {
+  public notaPedido: FormGroup;
+  public autorizarForm: FormGroup;
   menu;
   page = 1;
   pageSize = 3;
@@ -32,11 +36,57 @@ export class PedidosComponent implements OnInit {
   };
 
   constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
     private datePipe: DatePipe,
     private pedidosService: PedidosService,
     private paramService: ParamService,
   ) {
     this.inicio.setMonth(this.inicio.getMonth() - 3);
+    this.notaPedido = this.formBuilder.group({
+      facturacion: this.formBuilder.group({
+        nombres: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
+        apellidos: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
+        correo: ['', [Validators.required, Validators.email]],
+        identificacion: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        pais: ['', [Validators.required]],
+        provincia: ['', [Validators.required]],
+        ciudad: ['', [Validators.required]],
+        callePrincipal: ['', [Validators.required]],
+        numero: ['', [Validators.required]],
+        calleSecundaria: ['', [Validators.required]],
+        referencia: ['', [Validators.required]],
+        gps: ['', [Validators.required]],
+        codigoVendedor: ['', [Validators.required]],
+        nombreVendedor: ['', [Validators.required]],
+      }),
+      envio: this.formBuilder.group({
+        nombres: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
+        apellidos: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
+        correo: ['', [Validators.required, Validators.email]],
+        identificacion: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        telefono: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+        pais: ['', [Validators.required]],
+        provincia: ['', [Validators.required]],
+        ciudad: ['', [Validators.required]],
+        callePrincipal: ['', [Validators.required]],
+        numero: ['', [Validators.required]],
+        calleSecundaria: ['', [Validators.required]],
+        referencia: ['', [Validators.required]],
+        gps: ['', [Validators.required]],
+      }),
+      articulos: this.formBuilder.array([
+        this.crearDetalleGrupo()
+      ], Validators.required),
+    });
+    this.autorizarForm = this.formBuilder.group({
+      id: ['', [Validators.required]],
+      metodoConfirmacion: ['', [Validators.required]],
+      codigoConfirmacion: ['', [Validators.required]],
+      fechaHoraConfirmacion: ['', [Validators.required]],
+      tipoFacturacion: ['', [Validators.required]],
+    });
   }
 
   ngOnInit(): void {
@@ -47,6 +97,42 @@ export class PedidosComponent implements OnInit {
     this.barChartData = [this.datosTransferencias];
     this.obtenerTransacciones();
     this.obtenerOpciones();
+  }
+
+  get notaPedidoForm() {
+    return this.notaPedido['controls'];
+  }
+
+  get facturacionForm() {
+    return this.notaPedido.get('facturacion')['controls'];
+  }
+
+  get envioForm() {
+    return this.notaPedido.get('envio')['controls'];
+  }
+
+  get detallesArray(): FormArray {
+    return this.notaPedido.get('articulos') as FormArray;
+  }
+
+  crearDetalleGrupo(): any {
+    return this.formBuilder.group({
+      codigo: ['', [Validators.required]],
+      articulo: ['', [Validators.required]],
+      valorUnitario: [0, [Validators.required]],
+      cantidad: [0, [Validators.required, Validators.pattern('^[0-9]*$'), Validators.min(1)]],
+      precio: [0, [Validators.required]],
+    });
+  }
+
+  agregarItem(): void {
+    this.detallesArray.push(this.crearDetalleGrupo());
+  }
+
+  removerItem(i): void {
+    // this.calcularSubtotal();
+    this.detallesArray.removeAt(i);
+    console.log('form', this.notaPedidoForm);
   }
 
   obtenerTransacciones(): void {
@@ -69,6 +155,7 @@ export class PedidosComponent implements OnInit {
   obtenerTransaccion(id): void {
     this.pedidosService.obtenerPedido(id).subscribe((info) => {
       this.transaccion = info;
+      this.notaPedido.patchValue({...info});
       console.log(info);
     });
   }
@@ -80,12 +167,20 @@ export class PedidosComponent implements OnInit {
     });
   }
 
-  procesarEnvio(id, estado): void {
-    if (confirm('Esta seguro de cambiar de estado') === true) {
-      this.pedidosService.actualizarPedido({id, estado}).subscribe((info) => {
-        this.transaccion = info;
-        this.obtenerTransacciones();
-      });
-    }
+  procesarEnvio(modal, id): void {
+    this.modalService.open(modal);
+    this.autorizarForm = this.formBuilder.group({
+      id: [id, [Validators.required]],
+      metodoConfirmacion: ['', [Validators.required]],
+      codigoConfirmacion: ['', [Validators.required]],
+      fechaHoraConfirmacion: [ new Date(), [Validators.required]],
+      tipoFacturacion: ['', [Validators.required]],
+    });
+    // if (confirm('Esta seguro de cambiar de estado') === true) {
+    //   this.pedidosService.actualizarPedido({id, estado}).subscribe((info) => {
+    //     this.transaccion = info;
+    //     this.obtenerTransacciones();
+    //   });
+    // }
   }
 }
