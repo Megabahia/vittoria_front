88d56dd41fbdfafa2fe56cb43fe9e6730f3aa4e5
jimmy <rootkaliente@gmail.com>
2024-03-05 14:12:18 -0500

    [fixed]: mostrar datos de la factura

--- src/app/views/mp/pedidos/pedidos.component.ts
+++ src/app/views/mp/pedidos/pedidos.component.ts
@@ -4,8 +4,10 @@ import {Color, Label} from 'ng2-charts';
 import {DatePipe} from '@angular/common';
 import {PedidosService} from '../../../services/mp/pedidos/pedidos.service';
 import {ParamService} from '../../../services/mp/param/param.service';
+import {ParamService as ParamServiceMDP} from '../../../services/mdp/param/param.service';
 import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {NgbModal, NgbPagination} from '@ng-bootstrap/ng-bootstrap';
+import {ProductosService} from '../../../services/mdp/productos/productos.service';
 
 @Component({
   selector: 'app-pedidos',
@@ -35,6 +37,7 @@ export class PedidosComponent implements OnInit, AfterViewInit {
   datosTransferencias = {
     data: [], label: 'Series A', fill: false, borderColor: 'rgb(75, 192, 192)'
   };
+  public iva;
 
   constructor(
     private modalService: NgbModal,
@@ -42,9 +45,12 @@ export class PedidosComponent implements OnInit, AfterViewInit {
     private datePipe: DatePipe,
     private pedidosService: PedidosService,
     private paramService: ParamService,
+    private paramServiceMDP: ParamServiceMDP,
+    private productosService: ProductosService,
   ) {
     this.inicio.setMonth(this.inicio.getMonth() - 3);
     this.notaPedido = this.formBuilder.group({
+      id: ['', [Validators.required]],
       facturacion: this.formBuilder.group({
         nombres: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
         apellidos: ['', [Validators.required, Validators.minLength(1), Validators.pattern('[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s]+')]],
@@ -80,6 +86,11 @@ export class PedidosComponent implements OnInit, AfterViewInit {
       articulos: this.formBuilder.array([
         this.crearDetalleGrupo()
       ], Validators.required),
+      total: ['', [Validators.required]],
+      subtotal: ['', [Validators.required]],
+      iva: ['', [Validators.required]],
+      numeroPedido: ['', [Validators.required]],
+      created_at: ['', [Validators.required]],
     });
     this.autorizarForm = this.formBuilder.group({
       id: ['', [Validators.required]],
@@ -165,9 +176,10 @@ export class PedidosComponent implements OnInit, AfterViewInit {
 
   obtenerTransaccion(id): void {
     this.pedidosService.obtenerPedido(id).subscribe((info) => {
-      this.transaccion = info;
-      this.notaPedido.patchValue({...info});
-      console.log(info);
+      // this.transaccion = info;
+      const iva = +(info.total * this.iva.valor).toFixed(2);
+      const total = iva + info.total;
+      this.notaPedido.patchValue({...info, subtotal: info.total, iva, total});
     });
   }
 
@@ -176,6 +188,61 @@ export class PedidosComponent implements OnInit, AfterViewInit {
     this.paramService.obtenerListaPadres('PEDIDO_ESTADO').subscribe((info) => {
       this.opciones = info;
     });
+    this.paramServiceMDP.obtenerParametroNombreTipo('ACTIVO', 'TIPO_IVA').subscribe((info) => {
+        this.iva = info;
+      },
+      (error) => {
+        alert('Iva no configurado');
+      }
+    );
+  }
+
+  obtenerProducto(i): void {
+    this.productosService.obtenerProductoPorCodigo({
+      codigoBarras: this.detallesArray.value[i].codigo
+    }).subscribe((info) => {
+      if (info.codigoBarras) {
+        // this.detallesArray.value[i].codigo = info.codigo;
+        console.log('dato', this.detallesArray.controls[i].get('articulo'));
+        this.detallesArray.controls[i].get('articulo').setValue(info.nombre);
+        this.detallesArray.controls[i].get('cantidad').setValue(1);
+        this.detallesArray.controls[i].get('valorUnitario').setValue(info.precioVentaA);
+        this.detallesArray.controls[i].get('precio').setValue(info.precioVentaA * 1);
+        this.calcular();
+      } else {
+        // this.comprobarProductos[i] = false;
+        // this.mensaje = 'No existe el producto a buscar';
+        // this.abrirModal(this.mensajeModal);
+      }
+    }, (error) => {
+
+    });
+  }
+
+  calcular(): void {
+    const detalles = this.detallesArray.controls;
+    let subtotal = 0;
+    detalles.forEach((item, index) => {
+      const valorUnitario = parseFloat(detalles[index].get('valorUnitario').value);
+      const cantidad = parseFloat(detalles[index].get('cantidad').value);
+      detalles[index].get('precio').setValue((cantidad * valorUnitario).toFixed(2));
+      subtotal += parseFloat(detalles[index].get('precio').value);
+    });
+    this.notaPedido.get('subtotal').setValue(subtotal);
+    const iva = +(subtotal * this.iva.valor).toFixed(2);
+    const total = iva + subtotal;
+    this.notaPedido.get('iva').setValue(iva);
+    this.notaPedido.get('total').setValue(total);
+  }
+
+  actualizar(): void {
+    this.calcular();
+    if (confirm('Esta seguro de actualizar los datos') === true) {
+      this.pedidosService.actualizarPedido(this.notaPedido.value).subscribe((info) => {
+        this.modalService.dismissAll();
+        this.obtenerTransacciones();
+      });
+    }
   }
 
   procesarEnvio(modal, id): void {
@@ -184,7 +251,7 @@ export class PedidosComponent implements OnInit, AfterViewInit {
       id: [id, [Validators.required]],
       metodoConfirmacion: ['', [Validators.required]],
       codigoConfirmacion: ['', [Validators.required]],
-      fechaHoraConfirmacion: [ this.datePipe.transform(new Date(), 'yyyy-MM-ddThh:mm:ss.SSSZ'), [Validators.required]],
+      fechaHoraConfirmacion: [this.datePipe.transform(new Date(), 'yyyy-MM-ddThh:mm:ss.SSSZ'), [Validators.required]],
       tipoFacturacion: ['', [Validators.required]],
       estado: ['Empacado', [Validators.required]],
     });
