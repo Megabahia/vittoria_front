36210dbf6ca289ceb91f91ea8aed9e83f3fc1915
jimmy <rootkaliente@gmail.com>
2024-03-27 12:41:12 -0500

    [fixed]: corregir la paginacion en la pantalla de buscar productos

--- src/app/views/mdp/productos/buscar-producto/buscar-producto.component.ts
+++ src/app/views/mdp/productos/buscar-producto/buscar-producto.component.ts
@@ -1,65 +1,81 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductosService } from '../../../../services/mdp/productos/productos.service';
-import { ExportService } from '../../../../services/admin/export.service';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {ProductosService} from '../../../../services/mdp/productos/productos.service';
+import {ExportService} from '../../../../services/admin/export.service';
+import {NgbPagination} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-buscar-producto',
   templateUrl: './buscar-producto.component.html'
 })
-export class BuscarProductoComponent implements OnInit {
+export class BuscarProductoComponent implements OnInit, AfterViewInit {
+  @ViewChild(NgbPagination) paginator: NgbPagination;
   menu;
   listaProductos;
   page = 1;
   pageSize: any = 3;
   maxSize;
   collectionSize;
-  codigoBarras="";
-  nombre="";
+  codigoBarras = '';
+  nombre = '';
   infoExportar;
+
   constructor(
-    private productosService:ProductosService,
-    private exportFile:ExportService
-  ) { }
+    private productosService: ProductosService,
+    private exportFile: ExportService
+  ) {
+  }
 
   ngOnInit(): void {
     this.menu = {
-      modulo:"mdp",
-      seccion: "prodBusq"
+      modulo: 'mdp',
+      seccion: 'prodBusq'
     };
     this.obtenerListaProductos();
   }
 
-  obtenerListaProductos(){
-    this.productosService.buscarListaProductos({
-      nombre:this.nombre,
-      codigoBarras:this.codigoBarras,
-        page:this.page-1,
-        page_size:this.pageSize
-    }).subscribe((info)=>{
-        this.listaProductos = info.info;
-        this.collectionSize = info.cont
+  ngAfterViewInit(): void {
+    this.iniciarPaginador();
+    this.obtenerListaProductos();
+  }
+
+  iniciarPaginador(): void {
+    this.paginator.pageChange.subscribe(() => {
+      this.obtenerListaProductos();
     });
   }
-  exportarExcel(){
+
+  obtenerListaProductos() {
+    this.productosService.buscarListaProductos({
+      nombre: this.nombre,
+      codigoBarras: this.codigoBarras,
+      page: this.page - 1,
+      page_size: this.pageSize
+    }).subscribe((info) => {
+      this.listaProductos = info.info;
+      this.collectionSize = info.cont;
+    });
+  }
+
+  exportarExcel() {
     this.infoExportar = [];
-        const headers = ['Código de Barras', 'Nombre', 'Categoría', 'Subcategoría', 'Stock', 'Estado'];
-        this.listaProductos.forEach((row: any) => {
-          
-          const values = [];
-          values.push(row['codigoBarras']);
-          values.push(row['nombre']);
-          values.push(row['categoria']);
-          values.push(row['subCategoria']);
-          values.push(row['stock']);
-          values.push(row['estado']);
-          this.infoExportar.push(values);
-        });
-        const reportData = {
-          title: 'Reporte de Productos',
-          data: this.infoExportar,
-          headers
-        };
-  
-        this.exportFile.exportExcel(reportData);
+    const headers = ['Código de Barras', 'Nombre', 'Categoría', 'Subcategoría', 'Stock', 'Estado'];
+    this.listaProductos.forEach((row: any) => {
+
+      const values = [];
+      values.push(row['codigoBarras']);
+      values.push(row['nombre']);
+      values.push(row['categoria']);
+      values.push(row['subCategoria']);
+      values.push(row['stock']);
+      values.push(row['estado']);
+      this.infoExportar.push(values);
+    });
+    const reportData = {
+      title: 'Reporte de Productos',
+      data: this.infoExportar,
+      headers
+    };
+
+    this.exportFile.exportExcel(reportData);
   }
 }
