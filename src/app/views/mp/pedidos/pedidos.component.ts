f0abc89c791f0b54ce19bfadb6c26c119f984ca7
jimmy <rootkaliente@gmail.com>
2024-07-18 17:36:00 -0500

    [fix]: agregar campo de nombre del articulo en el mostrar la nota de pedido

--- src/app/views/mp/pedidos/pedidos.component.ts
+++ src/app/views/mp/pedidos/pedidos.component.ts
@@ -323,7 +323,7 @@ export class PedidosComponent implements OnInit, AfterViewInit {
         } else {
           this.productosService.obtenerProductoPorCodigo(data).subscribe((info) => {
             this.detallesArray.controls[i].get('id').setValue(info.id);
-            this.detallesArray.controls[i].get('articulo').setValue(info.nombre);
+            this.detallesArray.controls[i].get('articulo').setValue(info.nombre || info.articulo);
             this.detallesArray.controls[i].get('cantidad').setValue(this.detallesArray.controls[i].get('cantidad').value ?? 1);
             const precioProducto = info.precio;
             this.detallesArray.controls[i].get('valorUnitario').setValue(precioProducto.toFixed(2));
