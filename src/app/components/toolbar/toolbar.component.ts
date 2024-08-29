ba67723f607a174ec1a38bec4507edb48a729f9b
apveliz <apvelizs@utn.edu.ec>
2024-08-29 15:30:49 -0500

    fix: eliminar localstorage

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -87,7 +87,6 @@ export class ToolbarComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     this.usuario = JSON.parse(localStorage.getItem('currentUser'));
-    console.log(this.usuario);
     this.acciones = this.usuario.acciones;
     // this.selectMenu();
   }
