bd6f9585fd493b291b4a0023e282c272585d65bb
apveliz <apvelizs@utn.edu.ec>
2024-09-05 16:33:41 -0500

    fix: redireccionar

--- src/app/views/gsb/crear-pedido-woocommerce/crear-pedido-woocomerce.component.ts
+++ src/app/views/gsb/crear-pedido-woocommerce/crear-pedido-woocomerce.component.ts
@@ -501,7 +501,6 @@ export class CrearPedidoWoocomerceComponent implements OnInit {
           this.numeroPedido.push(result.numeroPedido);
           this.toaster.open('Pedido guardado', {type: 'success'});
           this.mostrarContenidoPantalla = false;
-          localStorage.removeItem('productosWoocommerce');
         }, error => this.toaster.open('Error al guardar pedido', {type: 'danger'}));
       });
 
@@ -902,7 +901,6 @@ export class CrearPedidoWoocomerceComponent implements OnInit {
     delete formData.pedidos;
 
     localStorage.setItem('productoDataPedidoWoocommerce', JSON.stringify(formData));
-    localStorage.removeItem('productosWoocommerce');
 
     //window.open('#/gdp/pedidos', '_blank');
     window.open('#/gdp/pedidos');
@@ -910,7 +908,6 @@ export class CrearPedidoWoocomerceComponent implements OnInit {
 
   irInicio() {
     window.open('#/admin/management');
-    localStorage.removeItem('productosWoocommerce');
   }
 }
 
